65f005bd098bca81a15050583ff61df01b0f71e9
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-01-05 12:26:40 +0500

    fix bug

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { LoggerService } from '../common/logger/logger.service';
 import { SapModule } from '../sap/sap.module';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
-  imports: [SapModule],
+  imports: [SapModule, UploadModule],
   controllers: [UsersController],
   providers: [UsersService, LoggerService],
   exports: [UsersService],
